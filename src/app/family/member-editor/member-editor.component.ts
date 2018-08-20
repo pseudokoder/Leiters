beee68525b451a9b86b6ac493f75f309d082d4d8
Wes Leiter <pseudokoder@gmail.com>
2018-08-20 00:20:03 -0700

    surname class init

--- src/app/family/member-editor/member-editor.component.ts
+++ src/app/family/member-editor/member-editor.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-editor.component.css']
 })
 export class MemberEditorComponent implements OnInit {
+  pageTitle = 'Family Member Editor';
+  member = 'JW';
 
   constructor() { }
 
