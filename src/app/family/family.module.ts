b887516366b43d00228dbcad6f124ec71284a696
Wes Leiter <pseudokoder@gmail.com>
2018-08-20 00:44:43 -0700

    added family-members comp

--- src/app/family/family.module.ts
+++ src/app/family/family.module.ts
@@ -5,12 +5,13 @@ import { FamilyRoutingModule } from './family-routing.module';
 import { MemberEditorComponent } from './member-editor/member-editor.component';
 import { FamilyNamesComponent } from './family-names/family-names.component';
 import { FamilyComponent } from './family/family.component';
+import { FamilyMembersComponent } from './family-members/family-members.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FamilyRoutingModule
   ],
-  declarations: [MemberEditorComponent, FamilyNamesComponent, FamilyComponent]
+  declarations: [MemberEditorComponent, FamilyNamesComponent, FamilyComponent, FamilyMembersComponent]
 })
 export class FamilyModule { }
