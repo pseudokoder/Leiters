9f43d042b0ef13c425289b8936a7f4ea9579f26d
Wes Leiter <pseudokoder@gmail.com>
2018-08-20 10:50:53 -0700

    edited family-routing mod

--- src/app/family/family-routing.module.ts
+++ src/app/family/family-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FamilyComponent } from './family/family.component';
+import { FamilyNamesComponent } from './family-names/family-names.component';
+import { FamilyMembersComponent } from './family-members/family-members.component';
+import { MemberEditorComponent } from './member-editor/member-editor.component';
 
-const routes: Routes = [];
+const familyRoutes: Routes = [
+  { path: 'family', component: FamilyComponent },
+  { path: 'family-members', component: FamilyMembersComponent },
+//  { path: 'family-members/:id', component: FamilyMembersComponent },
+  { path: 'family-names', component: FamilyNamesComponent },
+  { path: 'member-editor', component: MemberEditorComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(familyRoutes)],
   exports: [RouterModule]
 })
 export class FamilyRoutingModule { }
