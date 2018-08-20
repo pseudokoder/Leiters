2c5a74bc0c20b2f218925768c66baf7001e34e77
Wes Leiter <pseudokoder@gmail.com>
2018-08-20 11:04:19 -0700

    added page-not-found comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material';
 import { FamilyModule } from './family/family.module';
 import { CoreModule } from './core/core.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
