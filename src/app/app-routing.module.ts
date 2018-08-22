b60cead8c10aceae64c675c946d74c3e247aa309
Wes Leiter <pseudokoder@gmail.com>
2018-08-22 11:22:37 -0700

    edited app.comp for router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: 'welcome', component: WelcomeComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'full'},
-//  { path: '**', component: PageNotFoundComponent },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
