040faf404d1b21e44ba7569f3d1c2cf8ac4d261e
Wes Leiter <pseudokoder@gmail.com>
2018-08-29 00:57:03 -0700

    working on family-names unfinished

--- src/app/family/family-names/family-names.component.ts
+++ src/app/family/family-names/family-names.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SURNAMES  } from '../mock-surnames';
+
 @Component({
   selector: 'app-family-names',
   templateUrl: './family-names.component.html',
   styleUrls: ['./family-names.component.css']
 })
 export class FamilyNamesComponent implements OnInit {
+  surnames = SURNAMES;
 
   constructor() { }
 
---
+++ src/app/family/mock-surnames.ts
@@ -0,0 +1,14 @@
+import { Surname } from './Surname';
+
+export const SURNAMES: Surname[] = [
+  { id: 11, name: 'Leiter' },
+  { id: 12, name: 'Ellis' },
+  { id: 13, name: 'Napolean' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Something' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
